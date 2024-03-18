47fb770e283a3bcef29b4a2c4bf450f37e073a62
Denis Carriere <carriere.denis@gmail.com>
2024-03-18 18:18:16 +0100

    update action wrappers

--- src/mirror.cpp
+++ src/mirror.cpp
@@ -12,17 +12,17 @@ void wram::mirror_system_ram()
 {
    auto global = get_global();
    const int64_t total_ram_bytes_reserved = global.total_ram_bytes_reserved;
-   const asset supply = eosio::token::get_supply(get_self(), RAM_SYMBOL.code());
+   const asset supply = get_supply(get_self(), RAM_SYMBOL.code());
    const int64_t delta = total_ram_bytes_reserved - supply.amount;
 
    // issue supply
    if (delta > 0) {
-      eosio::token::issue_action issue_act{get_self(), {get_self(), "active"_n}};
+      issue_action issue_act{get_self(), {get_self(), "active"_n}};
       issue_act.send(get_self(), asset{delta, RAM_SYMBOL}, "mirror ram");
 
    // retire supply
    } else if (delta < 0) {
-      eosio::token::retire_action retire_act{get_self(), {get_self(), "active"_n}};
+      retire_action retire_act{get_self(), {get_self(), "active"_n}};
       retire_act.send(asset{-delta, RAM_SYMBOL}, "mirror ram");
    }
 }
