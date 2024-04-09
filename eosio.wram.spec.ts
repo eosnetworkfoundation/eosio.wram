27f91dc5fbccb58489cc9f87c1d4734704492108
Denis Carriere <carriere.denis@gmail.com>
2024-04-09 15:55:22 +0200

    add get_sender

--- eosio.wram.spec.ts
+++ eosio.wram.spec.ts
@@ -352,4 +352,9 @@ describe(wram_contract, () => {
         const action = contracts.wram.actions.transfer([wram_contract, wram_contract, `0 ${RAM_SYMBOL}`, '']).send(wram_contract)
         await expectToThrow(action, 'eosio_assert: cannot transfer to self')
     })
+
+    test('issue::error - must be executed by contract', async () => {
+        const action = contracts.wram.actions.issue([wram_contract, `10000 ${RAM_SYMBOL}`, '']).send(wram_contract)
+        await expectToThrow(action, 'eosio_assert: must be executed by contract')
+    })
 })
